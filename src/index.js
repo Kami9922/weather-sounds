d52503936ea6d2781a0497d716b90347a8cae9f3
Vitalya <vitalikt_22@mail.ru>
2025-04-19 19:08:24 +0400

    feat: implement bg change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './index.scss'
+
 import summerSound from '../src/assets/sounds/summer.mp3'
 import rainSound from '../src/assets/sounds/rain.mp3'
 import winterSound from '../src/assets/sounds/winter.mp3'
+
+import summerBg from '../src/assets/backgrounds/summer-bg.jpg'
+import rainyBg from '../src/assets/backgrounds/rainy-bg.jpg'
+import winterBg from '../src/assets/backgrounds/winter-bg.jpg'
 
 const summerAudio = new Audio(summerSound)
 const rainAudio = new Audio(rainSound)
@@ -24,16 +29,36 @@ function toggleAudio(audio) {
 	currentAudio = audio
 }
 
+function changeBackground(weatherType) {
+	const container = document.querySelector('.container')
+	switch (weatherType) {
+		case 'summer':
+			container.style.backgroundImage = `url(${summerBg})`
+			break
+		case 'rain':
+			container.style.backgroundImage = `url(${rainyBg})`
+			break
+		case 'winter':
+			container.style.backgroundImage = `url(${winterBg})`
+			break
+		default:
+			container.style.backgroundImage = 'none'
+	}
+}
+
 document.getElementById('summer').addEventListener('click', () => {
 	toggleAudio(summerAudio)
+	changeBackground('summer')
 })
 
 document.getElementById('rain').addEventListener('click', () => {
 	toggleAudio(rainAudio)
+	changeBackground('rain')
 })
 
 document.getElementById('winter').addEventListener('click', () => {
 	toggleAudio(winterAudio)
+	changeBackground('winter')
 })
 
 const volumeSlider = document.getElementById('volume-slider')
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -67,6 +67,10 @@ module.exports = {
 					},
 				],
 			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource',
+			},
 		],
 	},
 }
